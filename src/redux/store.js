b64454a989e45c43b648ab872ecde2ea23e15bff
Ivan <110199ws@gmail.com>
2025-06-04 08:52:54 +0300

    deleted logic of persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import contactsReduser from "./contactSlice";
 import filtersReducer from "./filterSlice";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import contactsReducer from "./contactSlice";
 import { customRootReducer } from "./customRootSlice";
 
-const contactsPersistorConfig = {
-  key: "contacts",
-  storage,
-};
-
-const persistContactsReducer = persistReducer(
-  contactsPersistorConfig,
-  contactsReduser
-);
-
 const rootReducer = combineReducers({
-  contacts: persistContactsReducer,
+  contacts: contactsReducer,
   filters: filtersReducer,
   loading: customRootReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
-export const persistor = persistStore(store);
